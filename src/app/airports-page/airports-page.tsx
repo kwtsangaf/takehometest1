1f5132d4033d9dd9efad05b73ae8e6782685c54b
raytsangdev <raytsangdev@gmail.com>
2023-02-13 17:52:04 -0500

    feat: set up more details for the map modal

--- src/app/airports-page/airports-page.tsx
+++ src/app/airports-page/airports-page.tsx
@@ -1,14 +1,23 @@
 import styles from "./airports-page.module.scss";
 import {useEffect, useState} from "react";
 import {getAirportsData} from "../data-service";
-import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 import {DataTypes} from "../types";
+import MapModal from "../map-modal/map-modal";
 
 /* eslint-disable-next-line */
 export interface AirportsPageProps {
 }
 
 export function AirportsPage(props: AirportsPageProps) {
+    const [open, setOpen] = useState(false);
+    const [count, setCounter] = useState(0);
+    const handleOpen = () => {
+        setOpen(true);
+        setCounter(count+1);
+    };
+    const handleClose = () => setOpen(false);
+
     const [data, setData] = useState<DataTypes>({features: [], type: ""});
 
     useEffect(() => {
@@ -19,27 +28,33 @@ export function AirportsPage(props: AirportsPageProps) {
         );
     }, []);
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: 650}} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Geo Type</TableCell>
-                        <TableCell align="right">Coor.</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.features.slice(0, 10).map((row, index) => (
-                        <TableRow
-                            key={row.type + index}
-                            sx={{"&:last-child td, &:last-child th": {border: 0}}}
-                        >
-                            <TableCell align="right">{row?.geometry.type}</TableCell>
-                            <TableCell align="right">{row?.geometry.coordinates}</TableCell>
+        <>
+            <MapModal open={open} count={count} handleClose={handleClose}></MapModal>
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 650}} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Geo Type</TableCell>
+                            <TableCell align="right">Coor.</TableCell>
+                            <TableCell align="right"></TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {data.features.slice(0, 10).map((row, index) => (
+                            <TableRow
+                                key={row.type + index}
+                                sx={{"&:last-child td, &:last-child th": {border: 0}}}
+                            >
+                                <TableCell align="right">{row?.geometry.type}</TableCell>
+                                <TableCell align="right">{row?.geometry.coordinates}</TableCell>
+                                <TableCell align="right"><Button variant="outlined" onClick={handleOpen}>Details</Button></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
+
     );
 }
 
--- src/app/map-modal/map-modal.tsx
+++ src/app/map-modal/map-modal.tsx
@@ -1,6 +1,7 @@
 import styles from "./map-modal.module.scss";
 import {Box, Button, Modal, Typography} from "@mui/material";
 import {useEffect, useState} from "react";
+import Map, {Layer, Source} from "react-map-gl";
 
 const style = {
     position: "absolute" as const,
@@ -21,6 +22,81 @@ export interface MapModalProps {
     count: number;
 }
 
+
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const token = import.meta.env.VITE_MAPBOX_TOKEN;
+
+const geojson: any = {
+    type: "FeatureCollection",
+    features: [
+        {type: "Feature", geometry: {type: "Point", coordinates: [-122.4, 37.8]}},
+        {type: "Feature", geometry: {type: "Point", coordinates: [-122.41, 37.8]}},
+        {type: "Feature", geometry: {type: "Point", coordinates: [-122.4, 37.805]}}
+    ]
+};
+
+const geojson2: any = {
+    "type": "Feature",
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [-67.13734351262877, 45.137451890638886],
+                [-66.96466, 44.8097],
+                [-68.03252, 44.3252],
+                [-69.06, 43.98],
+                [-70.11617, 43.68405],
+                [-70.64573401557249, 43.090083319667144],
+                [-70.75102474636725, 43.08003225358635],
+                [-70.79761105007827, 43.21973948828747],
+                [-70.98176001655037, 43.36789581966826],
+                [-70.94416541205806, 43.46633942318431],
+                [-71.08482, 45.3052400000002],
+                [-70.6600225491012, 45.46022288673396],
+                [-70.30495378282376, 45.914794623389355],
+                [-70.00014034695016, 46.69317088478567],
+                [-69.23708614772835, 47.44777598732787],
+                [-68.90478084987546, 47.184794623394396],
+                [-68.23430497910454, 47.35462921812177],
+                [-67.79035274928509, 47.066248887716995],
+                [-67.79141211614706, 45.702585354182816],
+                [-67.13734351262877, 45.137451890638886]
+            ]
+        ]
+    }
+};
+
+const parkLayer: any = {
+    id: 'landuse_park',
+    type: 'fill',
+    source: 'mapbox',
+    'source-layer': 'landuse',
+    filter: ['==', 'class', 'park'],
+    paint: {
+        'fill-color': '#4E3FC8'
+    }
+};
+
+const layerStyle: any = {
+    id: "point",
+    type: "circle",
+    paint: {
+        "circle-radius": 10,
+        "circle-color": "#49bf00"
+    }
+};
+
+const layerStyle2: any = {
+    "id": "water",
+    "type": "fill",
+    "paint": {
+        "fill-color": "#00ffff"
+    }
+}
+
+
 export function MapModal(props: MapModalProps) {
     const [open, setOpen] = useState(false);
     const handleClose = () => {
@@ -40,15 +116,20 @@ export function MapModal(props: MapModalProps) {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
-                    <Typography id="modal-modal-title" variant="h6" component="h2">
-                        Text in a modal
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{mt: 2}}>
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                    </Typography>
-                    {props.count}
-                </Box>
+                <Map
+                    initialViewState={{
+                        longitude: -122.4,
+                        latitude: 37.8,
+                        zoom: 4
+                    }}
+                    style={{width: 600, height: 400}}
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                    mapboxAccessToken={token}
+                >
+                    <Source id="my-data" type="geojson" data={geojson}>
+                        <Layer {...layerStyle} />
+                    </Source>
+                </Map>
             </Modal>
         </div>
     );
